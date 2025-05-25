734fdbd186cc1af42ee2ef6cead0efdbf4147b29
iris-credot <tetairiscredot@gmail.com>
2025-05-26 00:25:49 +0200

    Added middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import Link from "next/link";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Recipe Viewer</h1>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-        <Link href="/recipe">View Recipes</Link>
-      </SignedIn>
+    
     </div>
   );
 }
